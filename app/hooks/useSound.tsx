814b8113d245275ca9b4d9743711fefbddfc7cdc
yasutaka-asai <yasutaka.asai@mail.benesse.co.jp>
2024-12-30 22:11:39 +0900

    抽選時の音声を選択できるように変更

--- app/hooks/useSound.tsx
+++ app/hooks/useSound.tsx
@@ -1,10 +1,20 @@
 import { useCallback } from 'react';
 
 export const useSound = () => {
-    const playSound = useCallback(async () => {
+    const playSound = useCallback(async (soundIndex: number) => {
+      const sounds = [
+        { src: 'drumroll_and_rollend.mp3'},
+        { src: 'drumroll.mp3'},
+        { src: 'rollend.mp3'},
+      ];
       // 演出時間の調整 → page.tsxのtimerのdurationを変更する
       // アニメーションの長さ → globals.cssのanimationのdurationを変更する
-      const audio = new Audio('drumroll_and_rollend.mp3');
+      const sound = sounds[soundIndex];
+      if (!sound) {
+        console.error('選択された音声ファイルが見つかりません');
+        return;
+      }
+      const audio = new Audio(sound.src);
       audio.play();
     }, []);
   
